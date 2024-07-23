929824d344fb745e72e9883811c849ad73478d7d
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2024-07-23 17:13:20 +0530

    added bank account schema

--- backend/db.js
+++ backend/db.js
@@ -29,5 +29,23 @@ const userSchema=new Schema({
         maxLength: 50
     }
 });
-const User=mongoose.model("User",userSchema);
-module.exports=User;
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+	User,
+  Account,
+};
