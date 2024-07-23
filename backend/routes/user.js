7a709fbd750eeaab0d8171bb452886a668292dc8
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2024-07-23 17:04:33 +0530

    add more user routes and authMiddlewares

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,6 +5,7 @@ const zod = require("zod");
 const { User } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const  { authMiddleware } = require("../middleware");
 
 const signupBody = zod.object({
     username: zod.string().email(),
@@ -83,3 +84,47 @@ router.post("/signin", async (req, res) => {
     })
 })
 module.exports = router;
+
+const updateBody=zod.object({
+    firstname:zod.string().optional(),
+    lastname:zod.string().optional(),
+    password:zod.string().optional(),
+})
+router.put("/",authMiddleware,async(req,res)=>{
+    const {success}=updateBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message:"Incorrect inputs"
+        })
+    }
+    await User.updateOne({ _id: req.userId }, req.body);
+	
+    res.json({
+        message: "Updated successfully"
+    })
+})
+
+router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
