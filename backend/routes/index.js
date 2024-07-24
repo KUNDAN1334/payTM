5d0e9aa1730886d494b6acb0923eb3a196dae530
KUNDAN1334 <kundan.22bcon1334@jecrcu.edu.in>
2024-07-24 16:40:00 +0530

    Step 10 to Step 13

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const useRouter=require("./user");
+const accountRouter=require("./account");
+
+
+router.use("/user",useRouter);
+router.use("/accounts",accountRouter);
 
 module.exports = router;
-router.use("/user",useRouter);
